68fb76bb55b1305c8550e74dd7df0f6d9f991e58
Jani Hyytiäinen <jani.hyytiainen@iki.fi>
2025-05-11 19:18:38 +0300

    drop first row in import instead of modifying the csv

--- init_database.sh
+++ init_database.sh
@@ -9,15 +9,13 @@ if [ -f "$NAME" ]; then
     echo "Skipping unzip as output csv file already exists"
 else
     unzip Ajoneuvorekisteri.zip
-    # remove heading as we already have the schema in import.sql
-    sed -i 1d $NAME
 fi
 
 rm -f ajoneuvodata.sqlite
 echo "Creating database..."
 sqlite3 ajoneuvodata.sqlite < schema.sql
 echo "Importing data..."
-sqlite3 ajoneuvodata.sqlite '.separator ;' '.import Ajoneuvojen_avoin_data_5_28.csv tieliikenne'
+sqlite3 ajoneuvodata.sqlite '.separator ;' '.import -skip 1 Ajoneuvojen_avoin_data_5_28.csv tieliikenne'
 echo "ajoneuvodata.sqlite created successfully in $SECONDS seconds"
 echo "Cleaning up..."
 rm *.zip *.csv
